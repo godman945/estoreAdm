4a9a91a4ee81795c42656a9dddb646222c5444a8
alexchen3 <alexchen3@RNB20030254.fareastone.com.tw>
2021-03-03 17:26:33 +0800

    init

---
+++ src/main/resources/static/js/index/vue.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function(){
+
+console.log("----------------------");
+
+
+
+
+
+
+		var data_address = {"A01": "中文","A02": "英文"};
+		console.log(data_address);
+	
+	
+		 var app3Start = new Vue({
+		  el: '#app3',
+		  data: {
+		    message: [
+		     'TEST1','TEST2','TEST3'
+		    ]
+		  },
+		   mounted: function(){
+			 console.log(this);
+		   },
+		   components: {
+		   
+		   }
+		});
+		
+		
+		Vue.component('my-component', {
+		  template: '#my-component'
+		});
+		
+		
+		let vm = new Vue({
+		    el:'#app9',
+		    template:'<v-btn block>Block Button</v-btn>'
+		})
+		
+	
+})
